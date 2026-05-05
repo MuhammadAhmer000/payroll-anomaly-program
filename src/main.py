1440022433fbe229f2506ad332eca0f3353225e6
Muhammad Ahmer <muahmer534@gmail.com>
2026-05-05 22:44:11 +0400

    added set_config functionality in main through wrapper function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
+# import statements
+from src.config_loader import set_config
 
 
+# TODO: add endpoint for /config here...
+def main_set_config():
+
+    # require: from src.config_loader import set_config
+    return set_config()
+
+
+# Note: main will eventually be left for testing, React will be the "main" frontend
+# TODO: add endpoint for /test here...
 def main():
-    return None
+
+    config = main_set_config()
+    
+
+
+
+
 
 
 if __name__ == "__main__":
