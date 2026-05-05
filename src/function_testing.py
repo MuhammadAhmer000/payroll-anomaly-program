be621db5c1967532d7799e17a06a532cf7103ca4
Muhammad Ahmer <muahmer534@gmail.com>
2026-05-05 22:31:46 +0400

    added config_loader.py test to function_testing.py

--- src/function_testing.py
+++ src/function_testing.py
@@ -1,6 +1,18 @@
+# import
+import yaml
+
 import config_loader
+import logging
 
-def try_config_loader():
-    config = config_loader.set_config()
-    print(config)
+# preamble
+logger = logging.getLogger(__name__)
 
+
+def test_config_loader():
+    try:
+        config = config_loader.set_config()
+        logger.info("test complete: configuration loaded successfully")
+        print(config)
+
+    except yaml.YAMLError as exc:
+        logger.exception("test failed: configuration failed to load")
