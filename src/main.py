5c040d7770ac870819eb7e25b6d2845dfc9543de
Muhammad Ahmer <muahmer534@gmail.com>
2026-05-17 01:43:13 +0400

    simplified src/main.py by moving functions to routes.py, wrapper.py, and state.py

--- src/main.py
+++ src/main.py
@@ -1,261 +1,30 @@
-# import statements
-import logging
-import pandas as pd
-from fastapi import FastAPI, HTTPException
-from starlette.responses import FileResponse, JSONResponse
-
-from src.config_loader import set_config, set_config_api
-from src.data_exportation import compute_zscore_output, output_database
-from src.data_ingestion import load_payroll, load_payroll_api
-from src.database_ingestion import load_db_credentials
-from src.database_ingestion import import_database
-from src.helper_function import split_sort_employee, get_numerical_cols
-from src.machine_learning_testing import unsupervised_machine_learning_results, get_ensemble_results
-from src.payroll_rule_testing import payroll_validation_wrapper
-from src.stats_testing import compute_zscore_deviation
+# Standard library
+import io
 import logging
 from pathlib import Path
-from fastapi.responses import StreamingResponse
-import io
 
-from src.trend_analysis import get_trend_analysis, root_cause_formatter
-
-# preamble
-logger = logging.getLogger(__name__)
-app = FastAPI()
+# Third party
+from fastapi import FastAPI, HTTPException, APIRouter, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import APIRouter, UploadFile
+from fastapi.responses import StreamingResponse
 
-# global variables
-global stored_df
-global stored_config
-global stored_results_df
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_methods=["*"],
-    allow_headers=["*"],
+# Local
+from src.routes import router
+from src.trend_analysis import get_trend_analysis, root_cause_formatter
+from src.wrapper import (
+    SET_CONFIG, LOAD_PAYROLL, LOAD_DB_CREDENTIALS,
+    IMPORT_DATABASE, ANALYSIS_WRAPPER, EXPORT_WRAPPER, EXPORT_DATABASE
 )
 
+# App initialisation
+logger = logging.getLogger(__name__)
+app = FastAPI()
 
-# TODO: add "create table" if it doesn't exist
+# Middleware
+app.add_middleware(...)
 
-
-# TODO: add endpoint for /config here...
-def SET_CONFIG():
-
-    # required imports: from src.config_loader import set_config
-    try:
-        config = set_config()
-    except EnvironmentError:
-        logging.info(f"MAIN: failed to load configuration")
-        print(f"MAIN: failed to load configuration")
-        raise
-
-    return config
-
-
-# TODO: add endpoint for /test here...
-def LOAD_PAYROLL(file_path: str):
-    try:
-        dataframe = load_payroll(file_path)
-    except Exception as e:                                     # TODO: write better exceptions
-        logging.info(f"MAIN: failed to load payroll data")
-        print(f"MAIN: failed to load payroll data")
-        raise
-
-    return dataframe
-
-def LOAD_DB_CREDENTIALS():
-    db_credentials = load_db_credentials()
-    return db_credentials
-
-
-def IMPORT_DATABASE(db_credentials, database_name, simulate_data=False):
-    dataframe = import_database(db_credentials, database_name, simulate_data)
-    return dataframe
-
-# TODO: implement this, along with querying and stuff
-# def CREATE_DATABASE(db_credentials):
-
-
-def PAYROLL_RULE_TESTING(dataframe, config):
-    return payroll_validation_wrapper(dataframe, config)
-
-
-def STATS_TESTING(dataframe, z_threshold):
-    return compute_zscore_deviation(dataframe, z_threshold)
-
-
-def ML_TESTING(emp_id, dataframe):
-    unsupervised_results_df = unsupervised_machine_learning_results(dataframe)
-    ensemble_results = []  # eventually remove this
-    ensemble_df = get_ensemble_results(emp_id, unsupervised_results_df, ensemble_results)
-    return unsupervised_results_df, ensemble_df
-
-
-def TREND_ANALYSIS_TESTING(emp_id, emp_df):
-    field_root_cause = []  # eventually remove this
-    numeric_cols = get_numerical_cols()
-    root_cause_df = get_trend_analysis(emp_id, emp_df, field_root_cause, numeric_cols)
-    logger.info(f"Root Cause Compiled: {len(root_cause_df)} records")
-
-    root_cause_df_output, root_cause_summary_output = root_cause_formatter(root_cause_df)
-
-    # === ROOT CAUSE DETAILS ===
-    if root_cause_df_output.empty:
-        logger.debug(pd.DataFrame(columns=root_cause_df_output.columns))
-    else:
-        logger.debug(root_cause_df_output)
-
-    # === ROOT CAUSE SUMMARY ===
-    logger.debug(root_cause_summary_output)
-
-    return root_cause_df_output, root_cause_summary_output
-
-# TODO: VERY IMPORTANT separate excel & database exportation later
-
-
-def ANALYSIS_WRAPPER(DATAFRAME, EMP_COL, MONTH_COL, config, output_path):
-    results = []
-    for emp_id, emp_df in split_sort_employee(DATAFRAME, EMP_COL, MONTH_COL):
-
-            logger.info(f"Beginning analysis process for employee {emp_id}")
-
-
-            logger.info(f"Beginning: payroll rule testing for {emp_id}")
-            rule_df = PAYROLL_RULE_TESTING(DATAFRAME, config)
-            # logger.info(f"payroll rule testing completed. Table: {len(rule_df)} records")
-            print(rule_df)
-
-
-            logger.info(f"Beginning: statistics testing for {emp_id}")
-            stats_df = STATS_TESTING(DATAFRAME, config["rule_threshold"]["z_score"])
-            logger.info(f"Baseline Calculated: {len(stats_df)} records")
-            print(stats_df)
-
-
-            logger.info(f"Beginning: machine learning testing for {emp_id}")
-            unsupervised_df, ensemble_df = ML_TESTING(emp_id, DATAFRAME)
-            print(unsupervised_df)
-            print(ensemble_df)
-
-            logger.info(f"Beginning: machine learning testing for {emp_id}")
-            root_cause_df, root_cause_summary = TREND_ANALYSIS_TESTING(emp_id, DATAFRAME)
-            print(root_cause_df)
-            print(root_cause_summary)
-
-            # df_container = [rule_df, stats_df, unsupervised_df, ensemble_df, root_cause_df, root_cause_summary]
-
-            # EXPORT_WRAPPER(emp_id, df_container, writer)
-            results.append({
-                "emp_id": emp_id,
-                "rule_df": rule_df,
-                "stats_df": stats_df,
-                "unsupervised_df": unsupervised_df,
-                "ensemble_df": ensemble_df,
-                "root_cause_df": root_cause_df,
-                "root_cause_summary": root_cause_summary,
-                "overall": (rule_df["Anomaly"].any() or stats_df["anomaly"].any() or unsupervised_df["Ensemble_Anomaly"].any())
-            })
-    return results
-
-
-def EXPORT_WRAPPER(results, output_path):
-    with pd.ExcelWriter(output_path, engine="openpyxl") as writer:
-        for r in results:
-            compute_zscore_output(
-                writer,
-                r["emp_id"],
-                r["rule_df"],
-                r["stats_df"],
-                r["unsupervised_df"],
-                r["ensemble_df"],
-                r["root_cause_df"],
-                r["root_cause_summary"],
-            )
-    logger.info("Exportation has been completed in EXPORT_WRAPPER")
-
-
-def EXPORT_DATABASE(results, database_credentials):
-    output_database(results, database_credentials)
-    logger.info("dataset exported to database")
-
-
-@app.post("/download")
-def download_endpoint():
-    stored_results = stored_results_df
-
-    if not stored_results_df:
-        raise HTTPException(status_code=400, detail="No results available. Run analysis first.")
-
-    buffer = io.BytesIO()
-
-    with pd.ExcelWriter(buffer, engine="openpyxl") as writer:
-        for r in stored_results:
-            compute_zscore_output(
-                writer,
-                r["emp_id"],
-                r["rule_df"],
-                r["stats_df"],
-                r["unsupervised_df"],
-                r["ensemble_df"],
-                r["root_cause_df"],
-                r["root_cause_summary"],
-            )
-    logger.info("Exportation has been completed in download_endpoint")
-    buffer.seek(0)
-
-    return StreamingResponse(buffer, media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                             headers={"Content-Disposition": "attachment; filename=results.xlsx"})
-
-
-# TODO: change in the future to /payroll, not /upload
-@app.post("/upload")
-def upload_endpoint(file: UploadFile):
-    global stored_df
-    stored_df = load_payroll_api(file)
-    return {"status": "uploaded"}
-
-
-@app.post("/config")
-def config_endpoint(file: UploadFile):
-    global stored_config
-    stored_config = set_config_api(file)
-    return {"status": "config uploaded"}
-
-
-# Note: main will eventually be left for testing, React will be the "main" frontend
-# TODO: add endpoint for /test here...
-@app.post("/analyze")
-def analyze():
-    global stored_df, stored_config, stored_results_df
-
-    config = stored_config
-    payroll_df = stored_df
-    output_path = "output.xlsx"
-
-    EMP_COL, MONTH_COL = "employee_id", "month"
-
-    # TODO: to remove the "global", add a try/except block
-    df_container = ANALYSIS_WRAPPER(payroll_df, EMP_COL, MONTH_COL, config, output_path)
-
-    stored_results_df = df_container
-
-    return JSONResponse(content=[
-        {
-            "emp_id": r["emp_id"],
-            "rule_df": r["rule_df"].to_dict(orient="records"),
-            "stats_df": r["stats_df"].to_dict(orient="records"),
-            "unsupervised_df": r["unsupervised_df"].to_dict(orient="records"),
-            "ensemble_df": r["ensemble_df"].to_dict(orient="records"),
-            "root_cause_df": r["root_cause_df"].to_dict(orient="records"),
-            "root_cause_summary": r["root_cause_summary"].to_dict(orient="records"),
-            "overall": bool(r["overall"]),
-        }
-        for r in df_container
-    ])
+# Routers
+app.include_router(router)
 
 
 def main():
